15b3306b5698bf3c12c58bb0ed14d5f148dae16a
Andy Truong <andyhtruong01@gmail.com>
2025-11-02 23:39:16 -0500

    sdfasdf

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 console.log('Happy developing ✨')
-const g = 1231231;
-// sdfas
-const e = 12;
-const c = 10;
-const x = 5;
-const y = 6;
-const z = 7;
 
-const a = 8;
-const b = 9;
-const d = 11;
-const f = 1231231;
+function meow() {
+    console.log("meow");
+}
 
 function greet() {
     console.log("Hello, world");
